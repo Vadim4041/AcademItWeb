f2570be98e545c74ae2e92a7b350058afa9f32e0
Vadim4041 <kozhevnikov404@yandex.ru>
2024-01-03 17:26:21 +0700

    updated assignment 3 "JavaScript"

--- Assignments/3_JavaScript/arrays/arrays.js
+++ Assignments/3_JavaScript/arrays/arrays.js
@@ -2,28 +2,40 @@
     const array1 = [43, 36, 39, 45, 47, 47, 25, 3, 42, 14, 25, 29, 30, 0, 27];
     console.log("Array: " + array1.join(", "));
 
-    array1.sort(function (e1, e2) {
-        return e2 - e1;
-    });
+    function sortArray(array) {
+        array.sort(function (e1, e2) {
+            return e2 - e1;
+        });
 
-    console.log("Array sorted in descending order: " + array1.join(", "));
+        return array
+    }
 
-    const elementsCount = 5;
+    console.log("Array sorted in descending order: " + sortArray(array1).join(", "));
 
-    const firstFiveElements = array1.slice(0, elementsCount);
-    console.log("First five elements of array: " + firstFiveElements.join(", "));
+    function firstFiveElements(array) {
+        const elementsCount = 5;
 
-    const lastFiveElements = array1.slice(array1.length - elementsCount);
-    console.log("Last five elements of array: " + lastFiveElements.join(", "));
+        return array.slice(0, elementsCount);
+    }
 
-    const evenNumbersSum = array1.reduce(function (evenNumbersSum, number) {
-        return number % 2 === 0 ? evenNumbersSum + number : evenNumbersSum;
-    }, 0);
+    console.log("First five elements of array: " + firstFiveElements(array1).join(", "));
 
-    console.log("Sum of array elements that have even value: " + evenNumbersSum);
-})();
+    function lastFiveElements(array) {
+        const elementsCount = 5;
 
-(function () {
+        return array.slice(-elementsCount);
+    }
+
+    console.log("Last five elements of array: " + lastFiveElements(array1).join(", "));
+
+    function evenNumbersSum(array) {
+        return array.reduce(function (evenNumbersSum, number) {
+            return number % 2 === 0 ? evenNumbersSum + number : evenNumbersSum;
+        }, 0);
+    }
+
+    console.log("Sum of array elements that have even value: " + evenNumbersSum(array1));
+
     const array2Length = 100;
     const array2 = [];
 
@@ -33,13 +45,15 @@
 
     console.log("New array: " + array2.join(", "));
 
-    const evenNumbersSquares = array2
-        .filter(function (number) {
-            return number % 2 === 0;
-        })
-        .map(function (number) {
-            return Math.pow(number, 2);
-        });
+    function evenNumbersSquares(array) {
+        return array
+            .filter((number) => {
+                return number % 2 === 0;
+            })
+            .map((number) => {
+                return Math.pow(number, 2);
+            });
+    }
 
-    console.log("Squared even numbers of new array: " + evenNumbersSquares.join(", "));
+    console.log("Squared even numbers of new array: " + evenNumbersSquares(array2).join(", "));
 })();
