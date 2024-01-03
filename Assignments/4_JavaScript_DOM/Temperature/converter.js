76920d4875a2b30369959174f2c843e3d924ac9d
Vadim4041 <kozhevnikov404@yandex.ru>
2024-01-03 18:33:01 +0700

    updated assignment 4 "Temperature"

--- Assignments/4_JavaScript_DOM/Temperature/converter.js
+++ Assignments/4_JavaScript_DOM/Temperature/converter.js
@@ -1,26 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const inputField = document.getElementById("input-Celsius");
-    const outputFieldKelvin = document.getElementById("output-Kelvin");
-    const outputFieldFahrenheit = document.getElementById("output-Fahrenheit");
-    const button = document.getElementById("button1");
+    const inputFieldCelsius = document.getElementById("input-celsius");
+    const outputFieldKelvin = document.getElementById("output-kelvin");
+    const outputFieldFahrenheit = document.getElementById("output-fahrenheit");
+    const button = document.getElementById("button");
+
+    button.addEventListener("focus", function () {
+        document.querySelector("span.error_message").style.display = "none";
+    });
+
+    function convertCelsiusToKelvin(degreesCelsius) {
+        return Number(degreesCelsius) + 273.15;
+    }
+
+    function convertCelsiusToFahrenheit(degreesCelsius) {
+        return degreesCelsius * 9 / 5 + 32;
+    }
 
     button.addEventListener("click", function () {
-        const inputTemperature = inputField.value.trim();
+        const inputTemperature = inputFieldCelsius.value.trim();
+
         outputFieldKelvin.value = "";
         outputFieldFahrenheit.value = "";
 
-        inputField.value = inputTemperature;
-
-        function convertCelsiusToKelvin(degreesCelsius) {
-            return Number(degreesCelsius) + 273.15;
+        if (inputTemperature.length === 0 || isNaN(Number(inputTemperature))) {
+            document.querySelector("span.error_message").style.display = "inline";
+            return;
         }
+
+        inputFieldCelsius.value = inputTemperature;
 
         outputFieldKelvin.value = convertCelsiusToKelvin(inputTemperature).toFixed(2);
 
-        function convertCelsiusToFahrenheit(degreesCelsius) {
-            return degreesCelsius * 9 / 5 + 32;
-        }
-
         outputFieldFahrenheit.value = convertCelsiusToFahrenheit(inputTemperature).toFixed(2);
-    })
+    });
 });
