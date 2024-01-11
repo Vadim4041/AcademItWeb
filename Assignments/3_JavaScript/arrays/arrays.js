8c7ff70e3377de17f33bd8a833ddd53eb051fd1d
Vadim4041 <kozhevnikov404@yandex.ru>
2024-01-11 12:54:49 +0700

    updated task 3 "JS arrays"

--- Assignments/3_JavaScript/arrays/arrays.js
+++ Assignments/3_JavaScript/arrays/arrays.js
@@ -1,40 +1,30 @@
-(function () {
+(() => {
     const array1 = [43, 36, 39, 45, 47, 47, 25, 3, 42, 14, 25, 29, 30, 0, 27];
     console.log("Array: " + array1.join(", "));
 
-    function sortArray(array) {
-        array.sort(function (e1, e2) {
-            return e2 - e1;
-        });
-
-        return array
+    function sortArray(numbers) {
+        return numbers.sort((number1, number2) => number2 - number1);
     }
 
     console.log("Array sorted in descending order: " + sortArray(array1).join(", "));
 
-    function firstFiveElements(array) {
-        const elementsCount = 5;
-
+    function returnFirstNElements(array, elementsCount) {
         return array.slice(0, elementsCount);
     }
 
-    console.log("First five elements of array: " + firstFiveElements(array1).join(", "));
+    console.log("First five elements of array: " + returnFirstNElements(array1, 5).join(", "));
 
-    function lastFiveElements(array) {
-        const elementsCount = 5;
-
+    function lastNElements(array, elementsCount) {
         return array.slice(-elementsCount);
     }
 
-    console.log("Last five elements of array: " + lastFiveElements(array1).join(", "));
+    console.log("Last five elements of array: " + lastNElements(array1, 5).join(", "));
 
-    function evenNumbersSum(array) {
-        return array.reduce(function (evenNumbersSum, number) {
-            return number % 2 === 0 ? evenNumbersSum + number : evenNumbersSum;
-        }, 0);
+    function sumUpEvenNumbers(numbers) {
+        return numbers.reduce((evenNumbersSum, number) => number % 2 === 0 ? evenNumbersSum + number : evenNumbersSum, 0);
     }
 
-    console.log("Sum of array elements that have even value: " + evenNumbersSum(array1));
+    console.log("Sum of array even numbers: " + sumUpEvenNumbers(array1));
 
     const array2Length = 100;
     const array2 = [];
@@ -45,15 +35,11 @@
 
     console.log("New array: " + array2.join(", "));
 
-    function evenNumbersSquares(array) {
-        return array
-            .filter((number) => {
-                return number % 2 === 0;
-            })
-            .map((number) => {
-                return Math.pow(number, 2);
-            });
+    function squareEvenNumbers(numbers) {
+        return numbers
+            .filter(number => number % 2 === 0)
+            .map((number) => number * number);
     }
 
-    console.log("Squared even numbers of new array: " + evenNumbersSquares(array2).join(", "));
+    console.log("Squared even numbers of new array: " + squareEvenNumbers(array2).join(", "));
 })();
