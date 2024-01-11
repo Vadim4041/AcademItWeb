8e59017e66480a3ba6d9d467ca3edc7af5f59318
Vadim4041 <kozhevnikov404@yandex.ru>
2024-01-11 14:52:23 +0700

    updated task 4 "Temperature"

--- Assignments/4_JavaScript_DOM/Temperature/converter.js
+++ Assignments/4_JavaScript_DOM/Temperature/converter.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const inputFieldCelsius = document.getElementById("input-celsius");
-    const outputFieldKelvin = document.getElementById("output-kelvin");
-    const outputFieldFahrenheit = document.getElementById("output-fahrenheit");
+    const celsiusInputField = document.getElementById("input-celsius");
+    const kelvinOutputField = document.getElementById("output-kelvin");
+    const fahrenheitOutputField = document.getElementById("output-fahrenheit");
     const button = document.getElementById("button");
 
     button.addEventListener("focus", function () {
@@ -17,20 +17,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     button.addEventListener("click", function () {
-        const inputTemperature = inputFieldCelsius.value.trim();
+        const celsiusTemperature = Number(celsiusInputField.value);
+        const errorMessage = document.querySelector("span.error_message");
 
-        outputFieldKelvin.value = "";
-        outputFieldFahrenheit.value = "";
+        kelvinOutputField.value = "";
+        fahrenheitOutputField.value = "";
 
-        if (inputTemperature.length === 0 || isNaN(Number(inputTemperature))) {
-            document.querySelector("span.error_message").style.display = "inline";
+        if (celsiusTemperature.length === 0 || isNaN(celsiusTemperature)) {
+            errorMessage.style.display = "inline";
             return;
         }
 
-        inputFieldCelsius.value = inputTemperature;
+        celsiusInputField.value = celsiusTemperature;
 
-        outputFieldKelvin.value = convertCelsiusToKelvin(inputTemperature).toFixed(2);
-
-        outputFieldFahrenheit.value = convertCelsiusToFahrenheit(inputTemperature).toFixed(2);
+        kelvinOutputField.value = convertCelsiusToKelvin(celsiusTemperature).toFixed(2);
+        fahrenheitOutputField.value = convertCelsiusToFahrenheit(celsiusTemperature).toFixed(2);
     });
 });
