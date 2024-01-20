c0aa4a6cdcf6a6b0c9ee83cbef99368a42bb6a54
Vadim4041 <kozhevnikov404@yandex.ru>
2024-01-20 16:26:27 +0700

    updated assignment 7 "Todo list Vue" (not complete)

--- Assignments/7_Vue/Todo list/script.js
+++ Assignments/7_Vue/Todo list/script.js
@@ -2,21 +2,38 @@ Vue.createApp({})
     .component("TodoList", {
         data() {
             return {
-                items: []
+                items: [],
+                newTodoItemText: "",
+                newTodoItemId: 1
+            }
+        },
+
+        methods: {
+            addTodoItem() {
+                const newTodoItem = {
+                    id: this.newTodoItemId,
+                    text: this.newTodoItemText
+                };
+
+                this.newTodoItemId++;
+
+                this.items.push(newTodoItem);
+
+                this.newTodoItemText = "";
             }
         },
 
         template: `
-          <form class="row">
+          <form @submit.prevent="addTodoItem" class="row mb-3">
             <label class="col">
-              <input class="form-control" type="text">
+              <input v-model="newTodoItemText" class="form-control" type="text">
             </label>
             <div class="col-auto">
               <button class="btn btn-primary">Add</button>
             </div>
           </form>
 
-          <ul>
+          <ul class="list-unstyled">
             <todo-list-item v-for="item in items"
                             :key="item.id"
                             :item="item"></todo-list-item>
