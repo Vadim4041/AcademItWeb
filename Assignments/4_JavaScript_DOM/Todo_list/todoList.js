2f732ff931f4fa8878874fd5b8e6017e9ced4686
Vadim4041 <kozhevnikov404@yandex.ru>
2024-01-22 20:30:13 +0700

    updated assignment 4 "JS DOM Todo list"

--- Assignments/4_JavaScript_DOM/Todo_list/todoList.js
+++ Assignments/4_JavaScript_DOM/Todo_list/todoList.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const todoList = document.getElementById("todo_list_content");
-    const newNoteInputField = document.getElementById("new_note");
+    const newNoteInputField = document.getElementById("new_note_input");
     const addNewNoteButton = document.getElementById("add_new_note_button");
     const errorMessage = document.querySelector("span.error_message");
 
@@ -22,53 +22,55 @@ document.addEventListener("DOMContentLoaded", function () {
 
         function setEditMode() {
             todoNote.innerHTML = `<div class='note_block'>
-                <input class='edit_note' type='text'>
-                <span class='buttons_group'>
-                    <button class='list_button save_button' type='button'>save</button>
-                    <button class='list_button cancel_button' type='button'>cancel</button>
-                </span>
-                </div>`;
+                                      <input class='edit_note_input' type='text'>
+                                      <span class='buttons_group'>
+                                          <button class='list_button save_button' type='button'>Save</button>
+                                          <button class='list_button cancel_button' type='button'>Cancel</button>
+                                      </span>    
+                                      <div><span class="error_message error_edit" style="display: none;">Error: Note cannot be empty</span></div>            
+                                  </div>`;
 
-            let selectedNoteInputField = todoNote.querySelector(".edit_note");
+            const selectedNoteInputField = todoNote.querySelector(".edit_note_input");
             selectedNoteInputField.value = text;
 
             selectedNoteInputField.focus();
 
             todoNote.querySelector(".save_button").addEventListener("click", function () {
-                text = selectedNoteInputField.value.trim();
-
-                if (text.length === 0) {
-                    todoNote.remove()
-                    return;
-                }
-
-                setViewMode();
+                validateEditing();
             });
 
             selectedNoteInputField.addEventListener("keydown", function (e) {
                 if (e.key === "Enter" && selectedNoteInputField === document.activeElement) {
-                    text = selectedNoteInputField.value.trim();
-
-                    if (text.length === 0) {
-                        todoNote.remove()
-                        return;
-                    }
-
-                    setViewMode();
+                    validateEditing();
                 }
             });
 
             todoNote.querySelector(".cancel_button").addEventListener("click", setViewMode);
+
+            function validateEditing() {
+                const editError = todoNote.querySelector(".error_edit")
+                const validationText = selectedNoteInputField.value.trim();
+
+                if (validationText.length === 0) {
+                    editError.style.display = "block";
+                    selectedNoteInputField.classList.add("invalid_input");
+                    return;
+                }
+
+                text = validationText;
+
+                setViewMode();
+            }
         }
 
         function setViewMode() {
             todoNote.innerHTML = `<div class='note_block'>
-                <span class='todo_note'></span>
-                <span class='buttons_group'>
-                    <button class='list_button edit_button' type='button'>edit</button>
-                    <button class='list_button delete_button' type='button'>delete</button>
-                </span>
-                </div>`;
+                                      <span class='todo_note'></span>
+                                      <span class='buttons_group'>
+                                          <button class='list_button edit_button' type='button'>Edit</button>
+                                          <button class='list_button delete_button' type='button'>Delete</button>
+                                      </span>                
+                                  </div>`;
 
             todoNote.querySelector(".todo_note").textContent = text;
 
@@ -92,5 +94,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     addNewNoteButton.addEventListener("click", addNewNote);
-
 });
