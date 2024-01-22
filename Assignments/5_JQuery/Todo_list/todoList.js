e2022d23b8d0f9c5d64daa14d929155a17524218
Vadim4041 <kozhevnikov404@yandex.ru>
2024-01-22 20:52:08 +0700

    updated assignment 5 "Jquery Todo list"

--- Assignments/5_JQuery/Todo_list/todoList.js
+++ Assignments/5_JQuery/Todo_list/todoList.js
@@ -1,6 +1,6 @@
 $(function () {
     const todoList = $("#todo_list_content");
-    const newNoteInputField = $("#new_note");
+    const newNoteInputField = $("#new_note_input");
     const addNewNoteButton = $("#add_new_note_button");
     const errorMessage = $("span.error_message");
 
@@ -22,55 +22,57 @@ $(function () {
 
         function setEditMode() {
             todoNote.html(`<div class='note_block'>
-                <input class='edit_note' type='text'>
-                <span class='buttons_group'>
-                    <button class='list_button save_button' type='button'>save</button>
-                    <button class='list_button cancel_button' type='button'>cancel</button>
-                </span>
-                </div>`);
+                               <input class='edit_note_input' type='text'>
+                               <span class='buttons_group'>
+                                   <button class='list_button save_button' type='button'>Save</button>
+                                   <button class='list_button cancel_button' type='button'>Cancel</button>
+                               </span>
+                               <div><span class="error_message error_edit" style="display: none;">Error: Note cannot be empty</span></div>   
+                           </div>`);
 
-            let selectedNoteInputField = todoNote.find(".edit_note");
+            let selectedNoteInputField = todoNote.find(".edit_note_input");
             selectedNoteInputField.val(text);
 
             selectedNoteInputField.focus();
 
             todoNote.find(".save_button").click(function () {
-                text = selectedNoteInputField.val().trim();
-
-                if (text.length === 0) {
-                    todoNote.remove()
-                    return;
-                }
-
-                setViewMode();
+                validateEditing();
             });
 
             todoNote.keydown(function (event) {
                 const keyCode = (event.keyCode || event.which);
 
                 if (keyCode === 13) {
-                    text = selectedNoteInputField.val().trim();
-
-                    if (text.length === 0) {
-                        todoNote.remove()
-                        return;
-                    }
-
-                    setViewMode();
+                    validateEditing();
                 }
             });
 
             todoNote.find(".cancel_button").click(setViewMode);
+
+            function validateEditing() {
+                const editError = todoNote.find(".error_edit")
+                const validationText = selectedNoteInputField.val().trim();
+
+                if (validationText.length === 0) {
+                    editError.show();
+                    selectedNoteInputField.addClass("invalid_input");
+                    return;
+                }
+
+                text = validationText;
+
+                setViewMode();
+            }
         }
 
         function setViewMode() {
             todoNote.html(`<div class='note_block'>
-                <span class='todo_note'></span>
-                <span class='buttons_group'>
-                    <button class='list_button edit_button' type='button'>edit</button>
-                    <button class='list_button delete_button' type='button'>delete</button>
-                </span>
-                </div>`);
+                               <span class='todo_note'></span>
+                               <span class='buttons_group'>
+                                   <button class='list_button edit_button' type='button'>Edit</button>
+                                   <button class='list_button delete_button' type='button'>Delete</button>
+                               </span>
+                           </div>`);
 
             todoNote.find(".todo_note").text(text);
 
