371cab9c4d9888cfac5ee3cf55c3abcda7db174c
Vadim4041 <kozhevnikov404@yandex.ru>
2024-02-21 11:55:23 +0700

    updated assignment 7 "Todo list Vue"

--- Assignments/7_Vue/Todo list/script.js
+++ Assignments/7_Vue/Todo list/script.js
@@ -36,26 +36,26 @@ Vue.createApp({})
         },
 
         template: `
-          <form @submit.prevent="addTodoItem" class="row mb-3 justify-content-between todo_list_form">
-            <label class="col add_new_note_input mt-2" :class="{'is-invalid':!isNewNoteValid}" id="label-for-input">
-              <input v-model.trim="newTodoItemText" class="form-control add_new_note_input" :class="{'is-invalid':!isNewNoteValid}" type="text">
-            </label>
-            <div class="col-auto">
-              <button class="btn btn-primary mt-2">Add</button>
-            </div>
+            <form @submit.prevent="addTodoItem" class="row mb-3 justify-content-between todo_list_form">
+                <label class="col add_new_note_input mt-2" :class="{'is-invalid': !isNewNoteValid}" id="label-for-input">
+                    <input v-model.trim="newTodoItemText" class="form-control add_new_note_input" :class="{'is-invalid': !isNewNoteValid}" type="text">
+                </label>
+                <div class="col-auto">
+                    <button class="btn btn-primary mt-2">Add</button>
+                </div>
 
-            <div class="col-auto invalid-feedback">
-              Error: Note cannot be empty
-            </div>
-          </form>
+                <div class="col-auto invalid-feedback">
+                    Error: Note cannot be empty
+                </div>
+            </form>
 
-          <ul class="list-unstyled">
-            <todo-list-item v-for="item in items"
-                            :key="item.id"
-                            :item="item"
-                            @save-item="item.text = $event"
-                            @delete-item="deleteTodoItem(item)"></todo-list-item>
-          </ul>`
+            <ul class="list-unstyled">
+                <todo-list-item v-for="item in items"
+                                :key="item.id"
+                                :item="item"
+                                @save-item="item.text = $event"
+                                @delete-item="deleteTodoItem(item)"></todo-list-item>
+            </ul>`
     })
     .component("TodoListItem", {
         props: {
@@ -77,9 +77,6 @@ Vue.createApp({})
 
         methods: {
             save() {
-                this.isEditing = false;
-                this.isEditedNoteValid = true;
-
                 if (this.editingText.trim().length === 0) {
                     this.isEditedNoteValid = false;
                     this.isEditing = true;
@@ -87,42 +84,46 @@ Vue.createApp({})
                     return;
                 }
 
+                this.isEditing = false;
+                this.isEditedNoteValid = true;
+
                 this.$emit("save-item", this.editingText);
             },
 
             cancel() {
                 this.isEditing = false;
                 this.editingText = this.item.text;
+                this.isEditedNoteValid = true;
             }
         },
 
         template: `
-          <li class="mb-2">
-            <div v-if="!isEditing" class="row">
-              <div class="col">
-                {{ item.text }}
-              </div>
+            <li class="mb-2">
+                <div v-if="!isEditing" class="row">
+                    <div class="col">
+                        {{ item.text }}
+                    </div>
 
-              <div class="col-auto">
-                <button @click="$emit('delete-item')" class="btn btn-danger me-2" type="button">Delete</button>
-                <button @click="isEditing = true" class="btn btn-primary" type="button">Edit</button>
-              </div>
-            </div>
+                    <div class="col-auto">
+                        <button @click="$emit('delete-item')" class="btn btn-danger me-2" type="button">Delete</button>
+                        <button @click="isEditing = true" class="btn btn-primary" type="button">Edit</button>
+                    </div>
+                </div>
 
-            <div v-else class="row">
-              <div class="col edit_note_input" :class="{'is-invalid': !isEditedNoteValid}">
-                <input v-model.trim="editingText" @keydown.enter="save" class="form-control edit_note_input" :class="{'is-invalid': !isEditedNoteValid}" type="text">
-              </div>
+                <div v-else class="row">
+                    <div class="col edit_note_input" :class="{'is-invalid': !isEditedNoteValid}">
+                        <input v-model.trim="editingText" @keydown.enter="save" class="form-control edit_note_input" :class="{'is-invalid': !isEditedNoteValid}" type="text">
+                    </div>
 
-              <div class="col-auto">
-                <button @click="cancel" class="btn btn-secondary me-2" type="button">Cancel</button>
-                <button @click="save" class="btn btn-primary" type="button">Save</button>
-              </div>
+                    <div class="col-auto">
+                        <button @click="cancel" class="btn btn-secondary me-2" type="button">Cancel</button>
+                        <button @click="save" class="btn btn-primary" type="button">Save</button>
+                    </div>
 
-              <div class="col-auto invalid-feedback">
-                Error: Note cannot be empty
-              </div>
-            </div>
-          </li>`
+                    <div class="col-auto invalid-feedback">
+                        Error: Note cannot be empty
+                    </div>
+                </div>
+            </li>`
     })
     .mount("#app");
